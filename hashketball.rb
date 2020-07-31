22563ec0ffcae23c819a46807d73099408a3267a
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-31 01:08:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -182,3 +182,17 @@ def player_stats(name)
   end
 end
 
+def big_shoe_rebounds
+  biggest = 0
+  rebounds = 0
+  game_hash.each do |both_teams, keys|
+    keys[:players].each do |player|
+      size = player[:shoe]
+      if size > biggest
+        biggest = size
+        rebounds = player[:rebounds]
+      end
+    end
+  end
+  rebounds
+end

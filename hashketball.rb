9ccdfc64e302499b2ddc9d8edfe915e68f4bf7de
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-31 00:43:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -168,7 +168,7 @@ def player_numbers(team_name)
   game_hash.each do |both_teams, keys|
     if keys[:team_name] == team_name
       return keys[:player].map do |x|  x[:number]
-      #binding.pry
+      binding.pry
     end
     end
 end
